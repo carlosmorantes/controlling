570e70b088761b9436c77cbe64c4301864d2be72
karlihnos <carlostenis20@hotmail.com>
2016-03-11 08:50:09 +0100

    add helper for menu(month)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,30 @@ module ApplicationHelper
     	direction = sort_direction == 'asc' ? 'desc' : 'asc'    	
     	column = sort_column
     	link_to title, params.merge(column: column, direction: direction)      
-    end    
+    end 
+
+    def get_month      
+      translator = {
+      	january: "Januar",
+      	february: "Februar",
+      	march: 'März',
+      	april: "April",
+      	may: "Mai",
+      	june: "June",
+      	july: "Juli",
+      	august: "August",
+      	september: "September",
+      	october: "Oktober",
+      	november: "November",
+      	december: "Dezember"
+      }
+      month = Date.today.strftime("%B").downcase.to_sym
+      month_name_de = translator[month]
+      month_number = Date.today.strftime("%m")
+      year = Date.today.strftime("%Y")
+      return result = { 
+      	  month_name: month_name_de, 
+          month_number: month_number }            
+    end   
 	
 end
