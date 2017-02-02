6ef7fc3dbb351b38c68d6e9351de97b3c40c651d
Carlos <carlostenis20@hotmail.com>
2017-02-02 18:59:14 +0100

    correct problem for 2017

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,22 +1,22 @@
 class ExpensesController < ApplicationController
   include ExpensesHelper
-  helper_method :sort_column, :sort_direction  
+  helper_method :sort_column, :sort_direction
   before_action :authenticate_user!, except: [:index] # helper from devise
   before_action :set_expense, except: [:index, :new, :create]
 
-  
+
 
   # GET /expenses
   def index
     month = params[:month]
-    days = get_number_of_days(month,2016)
+    days = get_number_of_days(month,2017)
     if month.present?
-      @expenses = Expense.where("date >= :start_date AND date <= :end_date", 
-                               start_date: '2016-'+month+'-01', 
-                               end_date: '2016-'+month+'-'+days).order(sort_column+' '+sort_direction).page(params[:page]).per(10)
-      @sum_per_month = @expenses.sum(:price)    
+      @expenses = Expense.where("date >= :start_date AND date <= :end_date",
+                               start_date: '2017-'+month+'-01',
+                               end_date: '2017-'+month+'-'+days).order(sort_column+' '+sort_direction).page(params[:page]).per(10)
+      @sum_per_month = @expenses.sum(:price)
     else
-      @expenses = Expense.order(sort_column+' '+sort_direction).page(params[:page]).per(10)  
+      @expenses = Expense.order(sort_column+' '+sort_direction).page(params[:page]).per(10)
     end
 
   end
@@ -35,11 +35,11 @@ class ExpensesController < ApplicationController
   	@expense = Expense.create(expenses_params)
 
     if @expense
-      redirect_to @expense, notice: 'Artikel erfolgreich angelegt'	                   	
+      redirect_to @expense, notice: 'Artikel erfolgreich angelegt'
     else
   	  render :new
   	end
-	   
+
   end
 
   # GET expenses/:id/edit
@@ -49,18 +49,18 @@ class ExpensesController < ApplicationController
   # PUT update/:id
   def update
     if @expense.update(expenses_params)
-      redirect_to @expense, notice: 'Artikel erfolgreich geändert'  
+      redirect_to @expense, notice: 'Artikel erfolgreich geändert'
     else
       render :edit
-    end	
+    end
   end
 
-  def destroy    
+  def destroy
     @expense.destroy
-    redirect_to :back, notice: 'Artikel erfolgreich gelöscht' 
+    redirect_to :back, notice: 'Artikel erfolgreich gelöscht'
   end
 
-  private	
+  private
 
   def set_expense
     @expense = Expense.find(params[:id])
@@ -75,13 +75,13 @@ class ExpensesController < ApplicationController
   end
 
   def sort_column
-    Expense.column_names.include?(params[:sort]) ? params[:sort] : "date" 
+    Expense.column_names.include?(params[:sort]) ? params[:sort] : "date"
   end
 
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
-  
+
 
 end
