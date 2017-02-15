5f3d46224d9b141cb274ae5ffae9a7750c6335d6
Carlos <carlostenis20@hotmail.com>
2017-02-15 21:36:09 +0100

    enable cost table for different user

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,7 +1,7 @@
 module AdminHelper
   # check permission
   def permission?
-    if current_user.email != 'info@carrillomarket.com'
+    if current_user.email != 'info@carrillomarket.com' && current_user.email != 'angy@carrillomarket.com'
       true
     else
       false
