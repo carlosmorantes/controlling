d1bef18f7cda9ff974e2b451ec9359a12ce98a72
Carlos <carlostenis20@hotmail.com>
2017-02-16 21:27:19 +0100

    optimize method with the roles

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,10 +1,6 @@
 module AdminHelper
   # check permission
-  def permission?
-    if current_user.email != 'info@carrillomarket.com' && current_user.email != 'angy@carrillomarket.com'
-      true
-    else
-      false
-    end
+  def permission?(role)
+    current_user.role == role ? true : false
   end
 end
